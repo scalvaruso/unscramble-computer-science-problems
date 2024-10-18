0a44087f36ccc26790246eafb6e393a3aa122542
scalvaruso <calvaruso.simone@gmail.com>
2024-10-18 16:57:36 +0100

    Added comments

--- Task0.py
+++ Task0.py
@@ -20,6 +20,14 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+# texts[0] is the first element in the list of texts which has three columns:
+# Sending telephone number (texts[0][0]), Receiving telephone number (texts[0][1]), Timestamp of the text message (texts[0][2]).
+# Using indices to call these elements.
+
 print(f"First record of texts, {texts[0][0]} texts {texts[0][1]} at time {texts[0][2]}")
-    
+
+# calls[-1] is the last element in the list of calls which has four columns:
+# Calling telephone number (calls[-1][0]), Receiving telephone number (calls[-1][1]), Start timestamp of the telephone call (calls[-1][2]), Duration of the telephone call in seconds (calls[-1][3]).
+# Using indices to call these elements.
+
 print(f"Last record of calls, {calls[-1][0]} calls {calls[-1][1]} at time {calls[-1][2]}, lasting {calls[-1][3]} seconds")
