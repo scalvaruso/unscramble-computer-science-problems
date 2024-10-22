e538d3f80080ce2c527d8aafdfbae7435211e4d0
scalvaruso <calvaruso.simone@gmail.com>
2024-10-22 18:13:38 +0100

    Task2 Solved

--- Task2.py
+++ Task2.py
@@ -20,3 +20,24 @@ Print a message:
 September 2016.".
 """
 
+total_time_per_number = {}  # Dictionary to track total time spent by each phone number
+total_time = 0  
+telephone_number = None
+
+# Add phone numbers and their call durations
+for call in calls:
+
+    # Update caller's total time
+    total_time_per_number[call[0]] = total_time_per_number.get(call[0], 0) + int(call[3])
+    # Update receiver's total time
+    total_time_per_number[call[1]] = total_time_per_number.get(call[1], 0) + int(call[3])
+
+    # Update the telephone number with the maximum time
+    if total_time_per_number[call[0]] > total_time:
+        total_time = total_time_per_number[call[0]]
+        telephone_number = call[0]
+    if total_time_per_number[call[1]] > total_time:
+        total_time = total_time_per_number[call[1]]
+        telephone_number = call[1]
+
+print(f"{telephone_number} spent the longest time, {total_time} seconds, on the phone during September 2016.")
